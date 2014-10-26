466b18f171578b3cf8285f0d9129d3f91032113c
freelin <hummelf@onid.oregonstate.edu>
2014-10-25 17:37:45 -0700

    added simple curses interface, now shows appointments

---
+++ interface.py
@@ -0,0 +1,45 @@
+"""
+Here is a basic curses interface - so far it does nothing other than show a list of fake appointments.
+"""
+
+import curses 
+
+class Appointment:
+	def __init__(self, time, date, student):
+		self.time = time
+		self.date = date
+		#self.advisor = advisor
+		self.student = student
+
+def main():
+	screen = curses.initscr()
+	curses.noecho()
+	curses.curs_set(0)
+	screen.keypad(1)
+
+	appointmentList = []
+
+	a = Appointment("12:30pm", "01-01-2001", "Student's name")
+
+	#Fill appointmentList with fake appointments
+	for x in range(0,10):
+		appointmentList.append(Appointment("12:3"+str(x), "01-01-200"+str(x), "Student-"+str(x)))
+
+	screen.addstr("Current Appointments:\n") 
+	screen.addstr(1, 0, "DATE")
+	screen.addstr(1, 15, "TIME")
+	screen.addstr(1, 25, "STUDENT")
+
+	
+	for x in range(0,10):
+		screen.addstr((2+x), 0, appointmentList[x].date)
+		screen.addstr((2+x), 15, appointmentList[x].time)
+		screen.addstr((2+x), 25, appointmentList[x].student)
+	while True: 
+	   event = screen.getch() 
+	   if event == ord("q"): break 
+	    
+	curses.endwin()
+
+if __name__ == "__main__":
+    main()
