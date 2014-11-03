de7e8e67a192fa15fb481897163fa67f82996e35
KabkaCaana <kangkabir@gmail.com>
2014-11-02 20:09:46 -0800

    changes made so that i don't need gmail smtp

--- emailTesting/test2.py
+++ emailTesting/test2.py
@@ -3,13 +3,13 @@ from email.MIMEText import MIMEText
 from email.MIMEImage import MIMEImage
 import smtplib
 
-FROM = "me"
+FROM = "kangk@onid.oregonstate.edu"
 TO = "kangkabir@gmail.com"
 msg = MIMEMultipart()
 msg.attach(MIMEText(file("text.txt").read()))
-mailer = smtplib.SMTP('smtp.gmail.com',587)
+mailer = smtplib.SMTP('mail.engr.oregonstate.edu')
 mailer.ehlo()
 mailer.starttls()
 mailer.login('kangkabir@gmail.com','SPaP1903')
-mailer.sendmail(FROM,TO,msg.as_string())
+mailer.sendmail(msg['From'], msg['To'], msg.as_string())
 mailer.close()
