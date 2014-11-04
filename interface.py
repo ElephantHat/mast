2a31ebfa7ebd80b232f4c3a2efc243375877549b
hummel-f <hummelf@onid.oregonstate.edu>
2014-11-03 17:16:38 -0800

    client now gets appointments from database

--- interface.py
+++ interface.py
@@ -4,47 +4,60 @@ Here is a basic curses interface - so far it does nothing other than show a list
 Make sure to exit using 'q' and not CTRL-C
 """
 
-import curses 
+import curses
+import MySQLdb
 
 class Appointment:
-	def __init__(self, time, date, student):
-		self.time = time
-		self.date = date
-		#self.advisor = advisor
-		self.student = student
+    def __init__(self, time, date, student):
+        self.time = time
+        self.date = date
+        #self.advisor = advisor
+        self.student = student
 
 def main():
-	screen = curses.initscr()
-	curses.noecho()
-	curses.curs_set(0)
-	screen.keypad(1)
+    #connect to db
 
-	appointmentList = []
+    advisor_name = raw_input("Enter your name: ")
 
-	a = Appointment("12:30pm", "01-01-2001", "Student's name")
+    db = MySQLdb.connect(host='mysql.eecs.oregonstate.edu',
+                         user='cs419-group1',
+                         passwd='RddS6d6jEvaEXY4J',
+                         db='cs419-group1')
+    #create db cursor object
+    cur = db.cursor()
+    appointment_list = []
+    cur.execute("SELECT * FROM appointment INNER JOIN advisor ON advisor.id=aid INNER JOIN student ON student.id=sid WHERE advisor.name='"+advisor_name+"'")
 
-	#Fill appointmentList with fake appointments
-	#TODO use sql queries to get real data from database and format accordingly
-	for x in range(0,10):
-		appointmentList.append(Appointment("12:3"+str(x), "01-01-200"+str(x), "Student-"+str(x)))
+    #put matching appointments in list
+    number_of_appointments = 0
+    appointment_list = []
+    for row in cur.fetchall():
+        apt = Appointment(row[1], row[0], row[9])
+        appointment_list.append(apt)
+        number_of_appointments += 1
 
-	#print header
-	screen.addstr("Current Appointments:\n") 
-	screen.addstr(1, 0, "DATE")
-	screen.addstr(1, 15, "TIME")
-	screen.addstr(1, 25, "STUDENT")
-	#print out each appointment
-	for x in range(0,10):
-		screen.addstr((2+x), 0, appointmentList[x].date)
-		screen.addstr((2+x), 15, appointmentList[x].time)
-		screen.addstr((2+x), 25, appointmentList[x].student)
+    screen = curses.initscr()
+    curses.noecho()
+    curses.curs_set(0)
+    screen.keypad(1)
 
-	#TODO: Add ability to select appointments with arrow keys
-	while True: 
-	   event = screen.getch() 
-	   if event == ord("q"): break 
-	    
-	curses.endwin()
+    #print header
+    screen.addstr("Current Appointments:\n") 
+    screen.addstr(1, 0, "DATE")
+    screen.addstr(1, 15, "TIME")
+    screen.addstr(1, 25, "STUDENT")
+    #print out each appointment
+    for x in range(0,number_of_appointments):
+        screen.addstr((2+x), 0, appointment_list[x].date)
+        screen.addstr((2+x), 15, appointment_list[x].time)
+        screen.addstr((2+x), 25, appointment_list[x].student)
+
+    #TODO: Add ability to select appointments with arrow keys
+    while True: 
+       event = screen.getch() 
+       if event == ord("q"): break 
+        
+    curses.endwin()    
 
 if __name__ == "__main__":
     main()
