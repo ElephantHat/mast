3f83b6809e2c47d2596283b56f3b1c9d954ea356
hummel-f <hummelf@onid.oregonstate.edu>
2014-11-07 22:03:17 -0800

    finished database interface for creating appointments

--- database.py
+++ database.py
@@ -12,4 +12,19 @@ def create_cursor():
 def get_appointments(cur, advisor_name):
 	cur.execute("SELECT * FROM appointment INNER JOIN advisor ON advisor.id=aid INNER JOIN student ON student.id=sid WHERE advisor.name='"+advisor_name+"'")
 
-def add_appointment(cur, advisor_name, student_name, )
+def add_appointment(cur, advisor_name, advisor_email, student_name, student_email, date, time):
+
+	#check for and add advisor
+	cur.execute("SELECT * FROM advisor WHERE email='"+advisor_email+"'")
+	if(cur.rowcount==0):
+		#no advisor with this email, insert advisor info to DB
+		cur.execute("INSERT INTO advisor(id, email, name) VALUES (DEFAULT,'"+advisor_email+"','"+advisor_name+"')")	
+
+	#check for and add student
+	cur.execute("SELECT * FROM student WHERE email='"+student_email+"'")
+	if(cur.rowcount==0):
+		#no student with this email, insert student info to DB
+		cur.execute("INSERT INTO student(id, email, name) VALUES (DEFAULT,'"+student_email+"','"+student_name+"')")
+
+	#add appointment
+	cur.execute("INSERT INTO appointment(apt_date, apt_time, aid, sid) VALUES ('"+date+"','"+time+"',(SELECT id FROM advisor WHERE email='"+advisor_email+"'),(SELECT id FROM student WHERE email='"+student_email+"'))")
