1c3338f358f181e7d07e16e01d846082fa498c76
hummel-f <hummelf@onid.oregonstate.edu>
2014-12-09 18:44:23 -0800

    added scrolling ability and added seperate file for curses functionality

--- interface.py
+++ interface.py
@@ -5,6 +5,7 @@ Make sure to exit using 'q' and not CTRL-C
 """
 
 import curses
+import curses_interface
 import MySQLdb
 import database
 import signal
@@ -22,50 +23,16 @@ def main():
     appointment_list = []
 
     #read advisor email from ~/.mastrc
-    f = open(home+"/.mastrc", "r")
-    advisor_email = f.readline()
-    f.close()
-    advisor_email = advisor_email.rstrip("\n")
+    #commenting out for testing...
+    #f = open(home+"/.mastrc", "r")
+    #advisor_email = f.readline()
+    #f.close()
+    #advisor_email = advisor_email.rstrip("\n")
 
-    #put matching appointments in list
-    number_of_appointments = 0
-    raw_appointments = database.get_appointments(advisor_email)
+    advisor_email = "advisor1@oregonstate.edu"
 
-    appointment_list = []
+    client = curses_interface.AppointmentsInterface(advisor_email)
 
-    for appointments in raw_appointments:
-        new_apt = Appointment(appointments[4], appointments[3], appointments[1])
-        appointment_list.append(new_apt)
-        number_of_appointments += 1
-
-    screen = curses.initscr()
-    curses.noecho()
-    curses.curs_set(0)
-    screen.keypad(1)
-
-    #print header
-    screen.addstr("Current Appointments:\n")
-    screen.addstr(1, 0, "DATE")
-    screen.addstr(1, 15, "TIME")
-    screen.addstr(1, 25, "STUDENT")
-    #print out each appointment
-    for x in range(0,number_of_appointments):
-        screen.addstr((2+x), 0, appointment_list[x].date)
-        screen.addstr((2+x), 15, appointment_list[x].time)
-        screen.addstr((2+x), 25, appointment_list[x].student)
-
-    #TODO: Add ability to select appointments with arrow keys
-    while True:
-       event = screen.getch()
-       if event == ord("q"): break
-
-    curses.endwin()
-
-def signal_handler(signal, frame):
-    print("CTRL-C: Goodbye...")
-    curses.endwin()
-    sys.exit(0)# Isn't this all the functionality needed for CTRL-C? --Kabir
-signal.signal(signal.SIGINT,signal_handler)
 
 if __name__ == "__main__":
     main()
